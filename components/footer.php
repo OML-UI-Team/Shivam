8eda63ba0814f9132678b2edcc15d281f0f7e841
OML-UI-Team <114146073+OML-UI-Team@users.noreply.github.com>
2022-09-22 15:19:37 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,55 @@
+<footer class="site-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12 col-xs-12 text-center">
+                            <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved </p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+
+        
+
+        <!-- Footer -->
+
+
+
+
+    </div>
+    <!-- End Page Wrap -->
+
+
+    <!-- JS -->
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery.localScroll.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/all.js"></script>
+
+
+
+    <script>
+        $(document).ready(function () {
+
+            $("#clientlogo").owlCarousel({
+                margin: 10,
+                loop: true,
+                autoPlay: true,
+                items: 5,
+                itemsDesktop: [1000, 5],
+                itemsDesktopSmall: [900, 3],
+                itemsTablet: [600, 2],
+                itemsMobile: false
+
+            });
+        });
+    </script>
+
+</body>
+
+
+</html>
