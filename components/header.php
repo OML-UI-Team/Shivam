8202afefb0a4a9d613dae069502a3369cbbf1069
Wahmad9406 <waseem.ahmad202@gmail.com>
2022-09-22 15:22:57 +0530

    component

---
+++ components/header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Shivam Brandwrapper India Private Limited</title>
+    <meta name="description" content="Manufacturer of duplex board box, metalized mono cartons & packing box in Delhi.">
+    <meta charset="utf-8">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style-responsive.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+</head>
+
+<body class="appear-animate">
+    <!-- Page Loader -->
+    <div class="page-loader">
+        <div class="loader">Loading...</div>
+    </div>
+    <!-- End Page Loader -->
+
+    <!-- Skip to Content -->
+    <a href="#main" class="btn skip-to-content">Skip to Content</a>
+    <!-- End Skip to Content -->
+
+    <!-- Page Wrap -->
+    <div class="page" id="top">
+    <div class="container">
+            <div class="row">
+
+                <nav class="main-nav dark transparent stick-fixed wow-menubar">
+                    <div class="full-wrapper relative clearfix">
+
+                        <!-- Logo ( * your text or image into link tag *) -->
+                        <div class="nav-logo-wrap local-scroll">
+                            <a href="index.php" class="logo">
+                                <img src="images/logo-white.png" alt="Company logo" />
+                            </a>
+                        </div>
+
+                        <!-- Mobile Menu Button -->
+                        <div class="mobile-nav" role="button" tabindex="0">
+                            <i class="fa fa-bars"></i>
+                            <span class="sr-only">Menu</span>
+                        </div>
+
+                        <!-- Main Menu -->
+                        <div class="inner-nav desktop-nav">
+                            <ul class="clearlist scroll-nav local-scroll">
+                                <li class="active"><a href="index.php">Home</a></li>
+                                <li><a href="about-us.php">About</a></li>
+                                <li><a href="services.php">Services</a></li>
+                                <li><a href="gallery.php">Gallery</a></li>
+                                <li><a href="contact.php">Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <!-- End Main Menu -->
+
+                    </div>
+                </nav>
+            </div>
+        </div>
